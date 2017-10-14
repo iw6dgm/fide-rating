23ee763864855dffa809da974c5b684b3580a43b
joshua <iw6dgm@tiscali.it>
2017-10-14 17:40:45 +0100

    Init repository

---
+++ README.md
@@ -0,0 +1,50 @@
+FIDE Rating parser
+==================
+
+Parse the standard XML full player rating list provided by FIDE and save data into a SQLite3 database.
+
+Setup
+-----
+
+Create a SQLite3 database `fide.db` and create `player` by using:
+
+```sql
+CREATE TABLE IF NOT EXISTS player (
+  fideid bigint not null primary key,
+  name text, country char(3),
+  sex char(1),
+  title text,
+  w_title text,
+  o_title text,
+  foa_title text,
+  rating integer,
+  games integer,
+  k smallint,
+  rapid_rating integer,
+  rapid_games integer,
+  rapid_k smallint,
+  blitz_rating integer,
+  blitz_games integer,
+  blitz_k smallint,
+  birthday integer,
+  flag text
+)
+```
+
+Download XML file from FIDE website and unpack it. File name should be `players_list_xml_foa.xml`.
+
+Download and install Go SQLite3 dependency as described in [here](https://github.com/mattn/go-sqlite3).
+
+Compile the source code and run the executable with no parameters.
+
+```bash
+go build fideparser.go
+./fideparser
+```
+
+**Warning** The overall process is very time-consuming and the resulting database is huge.
+
+TODO
+----
+
+FIDE Rating API: Web app to provide friendly APIs to query the FIDE dabatase created by the parser above.
---
+++ fideparser.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/xml"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"io/ioutil"
+)
+
+const (
+	PlayersFilename = `players_list_xml_foa.xml`
+	PlayersDB       = `fide.db`
+	DeleteSQL       = `DELETE FROM player`
+	InsertSQL       = `INSERT INTO player (fideid,name,country,sex,title,w_title,o_title,foa_title,rating,games,k,rapid_rating,rapid_games,rapid_k,blitz_rating,blitz_games,blitz_k,birthday,flag) VALUES (? /*not nullable*/,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`
+	SelectCountSQL  = `SELECT COUNT(*) FROM player`
+)
+
+type PlayersList struct {
+	XMLName xml.Name `xml:"playerslist"`
+	Players []Player `xml:"player"`
+}
+
+type Player struct {
+	FideId      uint64 `xml:"fideid"`
+	Name        string `xml:"name"`
+	Country     string `xml:"country"`
+	Sex         string `xml:"sex"`
+	Title       string `xml:"title"`
+	WTitle      string `xml:"w_title"`
+	OTitle      string `xml:"o_title"`
+	FoaTitle    string `xml:"foa_title"`
+	Rating      uint   `xml:"rating"`
+	Games       uint   `xml:"games"`
+	K           uint8  `xml:"k"`
+	RapidRating uint   `xml:"rapid_rating"`
+	RapidGames  uint   `xml:"rapid_games"`
+	RapidK      uint8  `xml:"rapid_k"`
+	BlitzRating uint   `xml:"blitz_rating"`
+	BlitzGames  uint   `xml:"blitz_games"`
+	BlitzK      uint8  `xml:"blitz_k"`
+	Birthday    uint16 `xml:"birthday"`
+	Flag        string `xml:"flag"`
+}
+
+func main() {
+	// Read XML file
+	content, err := ioutil.ReadFile(PlayersFilename)
+	checkErr(err)
+
+	pl := PlayersList{}
+	// Parse XML content
+	err = xml.Unmarshal(content, &pl)
+	checkErr(err)
+
+	// Open database connection
+	db, _ := sql.Open("sqlite3", PlayersDB)
+
+	// Clean up player table
+	db.Query(DeleteSQL)
+
+	// Set up prepared statement to insert data
+	stmt, _ := db.Prepare(InsertSQL)
+
+	// Loop through decoded XML input data
+	for _, p := range pl.Players {
+		stmt.Exec(
+			// FIDE id
+			p.FideId,
+			// Basic info
+			p.Name, p.Country, p.Sex,
+			// Titles
+			p.Title, p.WTitle, p.OTitle, p.FoaTitle,
+			// Standard (classic) rating and K
+			p.Rating, p.Games, p.K,
+			// Rapid (classic) rating and K
+			p.RapidRating, p.RapidGames, p.RapidK,
+			// Blitz (classic) rating and K
+			p.BlitzRating, p.BlitzGames, p.BlitzK,
+			// Extra info
+			p.Birthday, p.Flag)
+	}
+	var count int
+	row := db.QueryRow(SelectCountSQL)
+	row.Scan(&count)
+	fmt.Printf("Total n. player(s) loaded : %d\n", count)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
